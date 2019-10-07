a4ed9de75b1ec53503c0a21a9881a434dcbcec90
choimroc <choimroc@foxmail.com>
2019-10-07 19:06:35 +0800

    优化部分工具

--- src/main/java/com/choimroc/demo/tool/generator/CodeGenerator.java
+++ src/main/java/com/choimroc/demo/tool/generator/CodeGenerator.java
@@ -46,7 +46,7 @@ public class CodeGenerator {
         gc.setOutputDir(projectPath + "/src/main/java");
         gc.setServiceName("%sService");
         gc.setAuthor("choimroc");
-        gc.setOpen(true);
+        gc.setOpen(false);
         mpg.setGlobalConfig(gc);
 
         // 数据源配置
